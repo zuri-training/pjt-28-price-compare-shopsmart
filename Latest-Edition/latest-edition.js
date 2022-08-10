4b72f32caf635cb7578e03165dc6314c43e0fcb5
Viashima <72166830+clins10@users.noreply.github.com>
2022-08-10 02:13:05 +0100

    latest edition page

---
+++ Latest-Edition/latest-edition.js
@@ -0,0 +1,201 @@
+var show = true;
+  
+function showCategoryCheckboxes() {
+    var checkboxes = 
+        document.getElementById("checkBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+
+    }
+}
+
+// show Brands Checkboxes
+
+function showBrandsCheckboxes() {
+    var checkboxes = 
+        document.getElementById("brandsCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//price range
+
+function showPriceRangeCheckboxes() {
+    var checkboxes = 
+        document.getElementById("priceRangeCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+// Band-width
+
+function showBandWidthCheckboxes() {
+    var checkboxes = 
+        document.getElementById("bandWidthCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Capacity-ROM
+function showCapacityRomCheckboxes() {
+    var checkboxes = 
+        document.getElementById("capacityRomCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Device - RAM
+function showDeviceRamCheckboxes() {
+    var checkboxes = 
+        document.getElementById("deviceRamCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Online Shop 
+function showOnlineShopCheckboxes() {
+    var checkboxes = 
+        document.getElementById("onlineShopCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Special Features 
+function showSpecialFeaturesCheckboxes() {
+    var checkboxes = 
+        document.getElementById("specialFeaturesbrandsCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Color
+function showColorCheckboxes() {
+    var checkboxes = 
+        document.getElementById("colorsCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Core Processor
+function showCoreProcessorCheckboxes() {
+    var checkboxes = 
+        document.getElementById("coreProcessorCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Display Size
+function showDisplaySizeCheckboxes() {
+    var checkboxes = 
+        document.getElementById("displaySizeCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//OS
+function showOsCheckboxes() {
+    var checkboxes = 
+        document.getElementById("osCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+//Simcard Slot
+function showSimcardSlotCheckboxes() {
+    var checkboxes = 
+        document.getElementById("simcardSlotCheckBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+        
+    }
+}
+
+
+
